4d6932bd72509f82683ade4b335ddb2ba68ad94d
Ksenia Bochko <ksenia.bochko1409@gmail.com>
2026-04-22 18:17:11 +0500

    Фикс Swiper

--- firms.js
+++ firms.js
@@ -1,31 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let allFirms = document.querySelector(".firms");
-  let showMoreButton = document.querySelector(".more__btn");
-  let btnText = document.querySelector(".btn-text");
-  let iconMore = showMoreButton.querySelector(".more__icon");
+let allFirms = document.querySelector(".firms");
+let showMoreButton = document.querySelector(".more__btn");
+let btnText = document.querySelector(".btn-text");
+let iconMore = showMoreButton.querySelector(".more__icon");
 
-  showMoreButton.addEventListener("click", function () {
-    allFirms.classList.toggle("firms--open");
+showMoreButton.addEventListener("click", function () {
+  allFirms.classList.toggle("firms--open");
 
-    if (allFirms.classList.contains("firms--open")) {
-      btnText.textContent = "Скрыть";
-      iconMore.classList.add("rotate");
-    } else {
-      btnText.textContent = "Показать все";
-      iconMore.classList.remove("rotate");
-    }
-  });
+  if (allFirms.classList.contains("firms--open")) {
+    btnText.textContent = "Скрыть";
+    iconMore.classList.add("rotate");
+  } else {
+    btnText.textContent = "Показать все";
+    iconMore.classList.remove("rotate");
+  }
+});
 
-  if (window.innerWidth <= 768) {
-    new Swiper(".swiper", {
+
+
+let swiper = null;
+
+function initSwiper() {
+  if (window.innerWidth <= 768 && !swiper) {
+    swiper = new Swiper(".swiper", {
       slidesPerView: "auto",
       spaceBetween: 16,
       loop: true,
-
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
       },
     });
+  } else if (window.innerWidth > 768 && swiper) {
+    swiper.destroy(true, true);
+    swiper = null;
   }
-});
+}
+
+initSwiper();
+
+window.addEventListener("resize", initSwiper);
